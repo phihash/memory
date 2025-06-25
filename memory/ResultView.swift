25363cf128b2583511f511b0514f1113f9a14386
phihash <insharp0220@gmail.com>
2025-06-26 01:09:31 +0900

    素材追加

---
+++ memory/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  memory
+//
+//  Created by Mail I on 2025/06/25.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
